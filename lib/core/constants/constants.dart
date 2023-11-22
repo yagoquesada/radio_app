2f10fa27705d5454ebf74c0430fa3cbbf17dd6dd
Yago Quesada Floriach <79659924+yagoquesada@users.noreply.github.com>
2023-11-23 00:13:45 +0100

    Update api key

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,3 @@
 const String apiUrl = 'https://radio-world-75-000-worldwide-fm-radio-stations.p.rapidapi.com/station_by_country.php?country_id=51&page=1&limit=20&order=ASC';
-const String apiKey = 'cc1026c5bfmshe9dec526a9cfd88p1d190ajsn3624f8f0d8c2';
+const String apiKey = 'YOUR_API_KEY';
 const String apiHost = 'radio-world-75-000-worldwide-fm-radio-stations.p.rapidapi.com';
